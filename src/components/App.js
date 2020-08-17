bc1ce44a9328ec46ef3ae93ff57961071ead8b07
Alexandru <rammazzoti2000@gmail.com>
2020-08-17 12:08:45 +0200

    Implement the App component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BooksList from './BooksList';
+import BooksForm from './BooksForm';
+
+const App = () => (
+  <div>
+    <BooksList />
+    <BooksForm />
+  </div>
+);
+
+export default App;
