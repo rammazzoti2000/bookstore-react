c3a58e89c2d67489f92cddb2fe75978c16260c78
Alexandru <rammazzoti2000@gmail.com>
2020-08-18 12:21:52 +0200

    Make the BooksForm component stateful with title and the book category in its own state

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -7,27 +7,39 @@ const dropdownMenu = categories.map(category => (
   </option>
 ));
 
-const BooksForm = () => (
-  <div>
-    <form className="main-form">
-      <label htmlFor="book-title">
-        Book Title:
-        <input
-          type="text"
-          id="book-title"
-          placeholder="Enter Book Title"
-          required
-        />
-      </label>
-      <label htmlFor="book-category">
-        Book Category:
-        <select id="book-category">
-          { dropdownMenu }
-        </select>
-      </label>
-      <button type="submit" className="submit">Add New Book</button>
-    </form>
-  </div>
-);
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: 'Action',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="main-form">
+          <label htmlFor="book-title">
+            Book Title:
+            <input
+              type="text"
+              id="book-title"
+              placeholder="Enter Book Title"
+              required
+            />
+          </label>
+          <label htmlFor="book-category">
+            Book Category:
+            <select id="book-category">
+              { dropdownMenu }
+            </select>
+          </label>
+          <button type="submit" className="submit">Add New Book</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default BooksForm;
