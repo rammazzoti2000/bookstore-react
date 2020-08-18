63c51d6b7f296472a90ca55291d6d8890eb18644
Alexandru <rammazzoti2000@gmail.com>
2020-08-19 01:16:28 +0200

    Refactor book reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,14 @@
+export const CREATE_BOOK = 'CREATE_BOOK';
+export const REMOVE_BOOK = 'REMOVE_BOOK';
+
 const createBook = book => ({
   type: 'CREATE_BOOK',
   book,
 });
 
-const removeBook = bookIndex => ({
+const removeBook = id => ({
   type: 'REMOVE_BOOK',
-  bookIndex,
+  id,
 });
 
 export { createBook, removeBook };
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,19 +1,17 @@
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
+
 const bookStore = (state = [], action) => {
   switch (action.type) {
-    case 'CREATE_BOOK':
+    case CREATE_BOOK:
       return [
         ...state,
-        {
-          id: action.book.id,
-          title: action.book.title,
-          category: action.book.category,
-        },
+        action.book,
       ];
 
-    case 'REMOVE_BOOK':
+    case REMOVE_BOOK:
       return [
-        ...state.slice(0, action.bookIndex),
-        ...state.slice(action.bookIndex + 1, state.length),
+        ...state.slice(0, action.id),
+        ...state.slice(action.id + 1, state.length),
       ];
 
     default:
