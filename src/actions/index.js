c8e17648b2be77e5f59fef56bce893a58331df71
Alexandru <rammazzoti2000@gmail.com>
2020-08-19 01:48:51 +0200

    Fix bug in book reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,12 @@ export const CREATE_BOOK = 'CREATE_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
 
 const createBook = book => ({
-  type: 'CREATE_BOOK',
+  type: CREATE_BOOK,
   book,
 });
 
 const removeBook = book => ({
-  type: 'REMOVE_BOOK',
+  type: REMOVE_BOOK,
   book,
 });
 
